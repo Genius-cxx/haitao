ede3f4700fdc68e453c62582416a732725777b1d
cxx <896432960@qq.com>
2019-02-25 21:21:35 +0800

    修改了base.js

--- src/js/base.js
+++ src/js/base.js
@@ -1,24 +1,6 @@
 //cookie
 $(function () {
-    var strLis=`  <li><a href="#"><span></span>会员中心</a></li>
-                        <li><a href="#"><span></span>联系客服</a></li>
-                        <li><a href="#"><span></span>个人资料</a></li>
-                        <li><a href="#"><span></span>收货地址</a></li>
-                        <li><a href="#"><span></span>站内消息</a></li>
-                        <li><a href="#" class="outLogin"><span></span>退出登录</a></li>
-                   `;
-    if(!($.cookie("username")=="")){
-        $(".header_right>ul>li:eq(5)>ul").html(strLis);
-        $(".username").html($.cookie("username"));
-        $(".main_top>p").html($.cookie("username"))
-    }else{
-        $(".username").html(" 登录▾");
 
-    }
-    $(".outLogin").on("click",function(){
-        $.cookie("username","");
-        location.reload();
-    })
     //置顶
         var $height=$(".nav_right>span").offset().top;
         $(window).scroll(function(){
@@ -46,15 +28,43 @@ $(function () {
     }else{
         addNum=0;
     }
-    $(".right>a>span").html(addNum);
-    $("#add").on("click", function () {
-        addNum++;
+
+
+    var strLis=`  <li><a href="#"><span></span>会员中心</a></li>
+                        <li><a href="#"><span></span>联系客服</a></li>
+                        <li><a href="#"><span></span>个人资料</a></li>
+                        <li><a href="#"><span></span>收货地址</a></li>
+                        <li><a href="#"><span></span>站内消息</a></li>
+                        <li><a href="#" class="outLogin"><span></span>退出登录</a></li>
+                   `;
+    //如果cookie username不为空
+    if(!($.cookie("username")=="")){
         $.cookie("goodList",addNum,{ expires: 7 })
+        $(".header_right>ul>li:eq(5)>ul").html(strLis);
+        $(".username").html($.cookie("username"));
+        $(".main_top>p").html($.cookie("username"));
+
+        //点击商品数量++
+        $("#add").on("click", function () {
+            addNum++;
+            $.cookie("goodList",addNum,{ expires: 7 })
+            $(".right>a>span").html(addNum);
+            console.log(addNum);
+            console.log($.cookie("goodList"));
+        })
+
         $(".right>a>span").html(addNum);
-        console.log(addNum);
-        console.log($.cookie("goodList"));
-
-
+    }else{
+        
+        $(".username").html(" 登录▾");
+        $("#add").on("click",function(){
+            $.cookie("goodList",0)
+            alert(" 请先登录")
+        })
+    }
+    //点击退出清除信息
+    $(".outLogin").on("click",function(){
+        $.cookie("username","");
+        location.reload();
     })
-
 })
